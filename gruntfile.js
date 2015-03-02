41662f4f597c2d310951cfe20c7011cd7c62b786
gmlove <gracekinglau@gmail.com>
2015-03-02 17:37:38 +0800

    add mongoose for model(query) layer

--- gruntfile.js
+++ gruntfile.js
@@ -54,7 +54,95 @@ module.exports = function(grunt) {
 			'coverage.html' : {
 				src: ['coverage.html']
 			}
-		}
+		},
+		concurrent: {
+			dev: {
+				tasks: ['nodemon', 'node-inspector'],
+				options: {
+					logConcurrentOutput: true
+				}
+			},
+			debug: {
+				tasks: ['nodemon:dev', 'node-inspector'],
+				options: {
+					logConcurrentOutput: true
+				}
+			},
+			"debug-test": {
+				tasks: ['nodemon:debug-test', 'node-inspector', 'watch:testsJS'],
+				options: {
+					logConcurrentOutput: true
+				}
+			}
+		},
+		nodemon: {
+			'debug-test': {
+				script: '/usr/local/lib/node_modules/mocha/bin/_mocha',
+				options: {
+					nodeArgs: ['--debug=31001', '--debug-brk'],
+					args: ['tests/compiler_test.js'],
+					watch: ['tests'],
+					ext: 'js',
+					callback: function (nodemon) {
+						nodemon.on('log', function (event) {
+							console.log(event.colour);
+						});
+						// opens browser on initial server start
+						nodemon.on('config:update', function () {
+							// Delay before server listens on port
+							setTimeout(function() {
+								require('open')('http://localhost:31010/debug?port=31001');
+							}, 1000);
+						});
+					}
+				}
+			},
+			dev: {
+				script: 'msample.js',
+				options: {
+					nodeArgs: ['--debug=31001', '--debug-brk'],
+					env: {
+						PORT: '31000'
+					},
+					ignore: [
+						'public/**'
+					],
+					watch: ['node_modules/**/*.js', 'views/**/*.js', 'services/**/*.js', '*.js', 'models/**/*.js'],
+					ext: 'js',
+					callback: function (nodemon) {
+						nodemon.on('log', function (event) {
+							console.log(event.colour);
+						});
+						// opens browser on initial server start
+						nodemon.on('config:update', function () {
+							// Delay before server listens on port
+							setTimeout(function() {
+								require('open')('http://localhost:31010/debug?port=31001');
+							}, 1000);
+						});
+						// refreshes browser when server reboots
+						nodemon.on('restart', function () {
+							// Delay before server listens on port
+							setTimeout(function() {
+								require('fs').writeFileSync('.rebooted', 'rebooted');
+							}, 1000);
+						});
+					}
+				}
+			}
+		},
+		'node-inspector': {
+			custom: {
+				options: {
+					'web-port': 31010,
+					'web-host': 'localhost',
+					'debug-port': 31001,
+					'save-live-edit': true,
+					'no-preload': false,
+					'stack-trace-limit': 100
+				}
+			}
+		},
 	});
 
 	// Load NPM tasks
---
+++ msample.js
@@ -0,0 +1,205 @@
+'use strict';
+
+var path = require('path');
+var util = require('util');
+global.MONGOOSE_DRIVER_PATH = path.resolve(__dirname, './lib/mongoose-driver/');
+var mongoose = require('./lib/mdbgoose');
+var types = mongoose.SchemaTypes;
+
+var memorydb = require('./lib');
+var uuid = require('node-uuid');
+var Q = require('q');
+var should = require('should');
+var logger = require('pomelo-logger').getLogger('memorydb', __filename);
+
+Q.longStackSupport = true;
+
+
+// For distributed system, just run memorydb in each server with the same config, and each server will be a shard.
+var main = function(){
+
+	// memorydb's config
+	var config = {
+		//shard Id (optional)
+		_id : 'shard1',
+		// Center backend storage, must be same for shards in the same cluster
+		backend : 'mongoose',
+		backendConfig : {uri : 'mongodb://localhost/memorydb-test'},
+		// Used for backendLock, must be same for shards in the same cluster
+		redisConfig : {host : '127.0.0.1', port : 6379},
+	};
+
+	var doc = {_id : 1, name : 'rain', level : 1};
+
+	var autoconn = null;
+	return Q.fcall(function(){
+		// Start memorydb
+		logger.debug('start memorydb');
+		return memorydb.start(config);
+	})
+	.then(function(){
+		// Get autoConnection
+		logger.debug('create connection');
+		autoconn = memorydb.autoConnect();
+
+		return autoconn.execute(function(){
+			// Get collection
+			var User = autoconn.collection('user');
+			return Q.fcall(function(){
+				// Insert a doc
+				return User.insert(doc._id, doc);
+			})
+			.then(function(){
+				// find the doc
+				return User.find(doc._id)
+				.then(function(ret){
+					ret.should.eql(doc);
+				});
+			});
+		}); // Auto commit here
+	})
+	.then(function(){
+		return autoconn.execute(function(){
+			var User = autoconn.collection('user');
+			return Q.fcall(function(){
+				// Update one field
+				return User.update(doc._id, {level : 2});
+			}).then(function(){
+				// Find specified field
+				return User.find(doc._id, 'level')
+				.then(function(ret){
+					ret.should.eql({level : 2});
+				});
+			}).then(function(){
+				// Exception here!
+				throw new Error('Oops!');
+			});
+		}) // Rollback on exception
+		.catch(function(e){
+			e.message.should.eql('Oops!');
+		});
+	})
+	.then(function(){
+		return autoconn.execute(function(){
+			var User = autoconn.collection('user');
+			return Q.fcall(function(){
+				// doc should be rolled back
+				return User.find(doc._id, 'level')
+				.then(function(ret){
+					ret.should.eql({level : 1});
+				});
+			})
+			.then(function(){
+				// Remove the doc
+				return User.remove(doc._id);
+			}); // Auto commit here
+		});
+	})
+	.then(function(){
+		var id = null;
+		var Cat = mongoose.model('Cat', { _id: String, name: String, fullname: {first: String, second: String}, extra: types.Mixed});
+		Cat.findOne(function(err, doc){
+			logger.debug('test findOne: doc=%j, err=', doc, err);
+		});
+		Cat.findById('54f3fb251444dd96c0afbd6e', 'name', function(err, doc){
+			logger.debug('test findById: doc=%j, err=', doc, err);
+		});
+		return autoconn.execute(function(){
+			var kitty = new Cat({ _id: uuid.v4(), name: 'Zildjian', fullname: {first: 'firstname', second: 'second name'}, extra: {xx: 'extra val'} });
+			id = kitty.id;
+			return Q.ninvoke(kitty, 'save')
+			.then(function(){
+				logger.info('save callback arguments: ' + util.inspect(arguments));
+			})
+			.then(function(){
+				kitty.name = 'xxx';
+				kitty.fullname.first = 'firstxxx';
+				kitty.extra = {xx1: 'changed extra'};
+				return Q.ninvoke(kitty, 'save');
+			})
+			.then(function(){
+				return Q.nfcall(function(cb){
+					return Cat.findOneInMdb(id, cb);
+				}).then(function(ret){
+					logger.info('findOneInMdb kitty: %j', ret);
+				});
+			});
+		})
+		.catch(function(e){
+			logger.error('error caught: ', e);
+		})
+		.then(function(){
+			return autoconn.execute(function(){
+				return Q.nfcall(function(cb){
+					return Cat.findOneInMdb(id, cb);
+				})
+
+			})
+			.catch(function(e){
+				logger.error('error caught: ', e);
+			});
+		});
+	})
+	.catch(function(e){
+		logger.error('xxx error caught: ', e);
+	})
+	.then(function(){
+		// Close autoConnection
+		return Q.all([autoconn.close(), memorydb.stop()]);
+	});
+};
+
+if (require.main === module) {
+	return Q.fcall(function(){
+		return main();
+	})
+	.fin(function(){
+		process.exit();
+	});
+} else {
+
+var A = function(){}
+A.prototype.toString = function(){return 'a'};
+
+var o = {};
+o[new A()] = 'adsf';
+logger.info('%j', o);
+
+Q.nfcall(function(cb){
+	setTimeout(function(){
+		console.log('1');
+		cb();
+	}, 1000);
+})
+.then(function(){
+	return Q.nfcall(function(cb){
+		setTimeout(function(){
+			console.log('2');
+			throw new Error('error raised');
+			cb();
+		}, 1000);
+	});
+})
+.done(function(){
+	console.log('q done');
+}
+,function(e){
+	logger.error('error caught: ', e);
+})
+;
+
+/*
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/memorydb-test');
+var Cat = mongoose.model('Cat', { _id: String, name: String, fullname: {first: String, second: String}, extra: types.Mixed});
+Cat.findOne(function(err, doc){
+	logger.debug('test findOne: doc=%j, err=', doc, err);
+});
+Cat.findById('54f3fb251444dd96c0afbd6e', 'name', function(err, doc){
+	logger.debug('test findById: doc=%j, err=', doc, err);
+});
+setTimeout(function(){mongoose.disconnect();}, 5000);
+*/
+
+
+}
