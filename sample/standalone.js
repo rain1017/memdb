749debe834362181d58495abd47992f0695131ce
Yu Xia <rain1017@gmail.com>
2015-06-03 11:27:56 +0800

    update doc

--- sample/standalone.js
+++ sample/standalone.js
@@ -4,22 +4,36 @@
 // run with node >= 0.12 with --harmony option
 
 var memdb = require('memdb');
+var should = require('should');
 var P = require('bluebird');
 
 // IMPORTANT: You should first start memdb server by:
 // node app/server.js --conf=test/memdb.json --shard=s1
+// node app/server.js --conf=test/memdb.json --shard=s2
 
 var main = P.coroutine(function*(){
-    // When specifying port and host, client will connect to the standalone server
-    var autoconn = yield memdb.autoConnect({host : '127.0.0.1', port : 3000});
+    // When specifying shards config, client will connect to the standalone server
+    var shards = {
+            s1 : {host : '127.0.0.1', port : 3000},
+            s2 : {host : '127.0.0.1', port : 3001},
+        };
+    var autoconn = yield memdb.autoConnect({shards : shards});
 
-    // Make some query in one transaction
+    var Player = autoconn.collection('player');
+    var playerId = null;
+
+    // Make a transaction in shard s1
     yield autoconn.transaction(P.coroutine(function*(){
-        var Player = autoconn.collection('player');
-        var playerId = yield Player.insert({name : 'rain'});
-        console.log(yield Player.find(playerId));
+        playerId = yield Player.insert({name : 'rain'});
+    }), 's1');
+
+    // Make another transaction in shard s2
+    yield autoconn.transaction(P.coroutine(function*(){
+        var player = yield Player.find(playerId);
+        player.should.eql({_id : playerId, name : 'rain'});
+
         yield Player.remove(playerId);
-    }));
+    }), 's2');
 
     // Close the connection
     yield autoconn.close();
