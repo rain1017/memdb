9f091430abb4eb776d68cebc5f1501cc7de94312
Yu Xia <rain1017@gmail.com>
2015-08-07 16:56:24 +0800

    update node to >=v0.12.5

--- gruntfile.js
+++ gruntfile.js
@@ -56,14 +56,14 @@ module.exports = function(grunt) {
                     require: 'test/blanket'
                 }
             },
-            coverage: {
-                src: watchFiles.testJS,
-                options : {
-                    reporter: 'html-cov',
-                    quiet: true,
-                    captureFile: 'coverage.html'
-                }
-            }
+            // coverage: {
+            //     src: watchFiles.testJS,
+            //     options : {
+            //         reporter: 'html-cov',
+            //         quiet: true,
+            //         captureFile: 'coverage.html'
+            //     }
+            // }
         },
         clean: {
             'coverage.html' : {
